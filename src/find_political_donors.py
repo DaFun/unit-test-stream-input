c276cd1116285791e196a62b9c95880c86451291
Fei <beachibo@gmail.com>
2017-10-26 23:41:46 -0700

    median_by_date

--- src/find_political_donors.py
+++ src/find_political_donors.py
@@ -1,38 +1,98 @@
 #!/usr/bin/env python
 from median import Median
+import datetime
 
-
+# parse the relevant fields from line
 def process_data(line):
     fields = line.strip().split('|')
     return fields[0], fields[10][:5], fields[13], int(fields[14]), fields[15]
 
-with open('../input/test') as f, open('../output/test1', 'w') as out:
-    file_by_zip = {}
+
+# process median_by_zip file 
+def median_by_zip(dict_by_zip, cmte, zip, amt, out):
+    # get the zip dictionary according to the recipient id 
+    zip_dict = dict_by_zip.get(cmte, None)
+    
+    # if dict_by_zip already has the recipient record
+    if zip_dict:
+        # get the median object according to zip code 
+        median = zip_dict.get(zip, None)
+        if median:
+            median.add_num(amt)
+            ready_to_print_median = median.find_median()
+            ready_to_print_num = median.get_num()
+            ready_to_print_total = median.get_total()
+        else:
+            # if this recipient no this zip code record, then init a new Median obj for this zip code
+            median = Median()
+            median.add_num(amt)
+            zip_dict[zip] = median
+            ready_to_print_median = median.find_median()
+            ready_to_print_num = median.get_num()
+            ready_to_print_total = median.get_total()
+    else:
+        # dict_by_zip no this recipient record, init a new zip dictionary relate to this recipient id
+        # and init a new median obj, add the value
+        zip_dict = {}
+        median = Median()
+        median.add_num(amt)
+        zip_dict[zip] = median
+        dict_by_zip[cmte] = zip_dict
+        ready_to_print_median = median.find_median()
+        ready_to_print_num = median.get_num()
+        ready_to_print_total = median.get_total()
+        
+    # print out info according to the median object
+    out.write("{} | {} | {} | {} | {}\n".format(cmte, zip, ready_to_print_median, ready_to_print_num, ready_to_print_total))
+
+
+def median_by_date(dict_by_date, cmte, date, amt):
+    # get the records of this recipient
+    date_dict = dict_by_date.get(cmte, None)
+
+    # convert date string to datetime object in order to sort
+    dt = datetime.datetime.strptime(date, "%m%d%Y")
+
+    if date_dict:
+        median = date_dict.get(dt, None)
+
+        # if the record relate to this recipient at this date already be stored
+        if median:
+            median.add_num(amt)
+        else:
+            # init a new median object to store the values
+            median = Median()
+            median.add_num(amt)
+            date_dict[dt] = median
+    else:
+        # no records at this date have been found, then init a new dictionary for this date
+        date_dict = {}
+        median = Median()
+        median.add_num(amt)
+        date_dict[dt] = median
+        dict_by_date[cmte] = date_dict
+
+
+# print out median_by_date at the end
+def print_median_by_date(dict_by_date, out):
+    # sorted alphabetical by recipient
+    for cmte, v in sorted(dict_by_date.items()):
+
+        # sorted chronologically by date
+        for dt, median in sorted(v.items()):
+            date = dt.strftime("%m%d%Y")
+            ready_to_print_median = median.find_median()
+            ready_to_print_num = median.get_num()
+            ready_to_print_total = median.get_total()
+            out.write("{} | {} | {} | {} | {}\n".format(cmte, date, ready_to_print_median, ready_to_print_num, ready_to_print_total))
+
+
+with open('../input/test') as f, open('../output/test1', 'w') as out, open('../output/test2', 'w') as out1:
+    dict_by_zip = {}
+    dict_by_date = {}
     for line in f:
         cmte, zip, dt, amt, id = process_data(line)
         if not id:
-            zip_dict = file_by_zip.get(cmte, None)
-            if zip_dict:
-                median = zip_dict.get(zip, None)
-                if median:
-                    median.add_num(amt)
-                    out_median = median.find_median()
-                    out_num = median.get_num()
-                    out_total = median.get_total()
-                else:
-                    median = Median()
-                    median.add_num(amt)
-                    zip_dict[zip] = median
-                    out_median = median.find_median()
-                    out_num = median.get_num()
-                    out_total = median.get_total()
-            else:
-                zip_dict_tmp = {}
-                median = Median()
-                median.add_num(amt)
-                zip_dict_tmp[zip] = median
-                file_by_zip[cmte] = zip_dict_tmp
-                out_median = median.find_median()
-                out_num = median.get_num()
-                out_total = median.get_total()
-            out.write("{} | {} | {} | {} | {}\n".format(cmte, zip, out_median, out_num, out_total))
+            median_by_zip(dict_by_zip, cmte, zip, amt, out)
+            median_by_date(dict_by_date, cmte, dt, amt)
+    print_median_by_date(dict_by_date, out1)
--- src/median.py
+++ src/median.py
@@ -7,18 +7,29 @@ class Median:
         self.num = 0
 
     def add_num(self, num):
+        # smaller half and bigger half of values
         small, large = self.heaps
-        heappush(small, heappushpop(large, num))
+
+        # push inverted values into small, so make small as a max-heap
+        heappush(small, -heappushpop(large, num))
+
+        # keep large hold the median if odd elements
         if len(large) < len(small):
-            heappush(large, heappop(small))
+            heappush(large, -heappop(small))
         self.num += 1
         self.total += num
 
     def find_median(self):
         small, large = self.heaps
+
+        # odd elements in total
         if len(large) > len(small):
             return large[0]
-        return ((large[0] + small[0]) >> 1) + ((large[0] + small[0]) & 1)
+
+        # even elements in total
+        else:
+            # deal with round up, especially when meet big number
+            return ((large[0] - small[0]) >> 1) + ((large[0] + small[0]) & 1)
 
     def get_num(self):
         return self.num
