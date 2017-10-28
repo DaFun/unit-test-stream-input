36156061b1b026de3415c35015d006d442ae48bd
Fei <beachibo@gmail.com>
2017-10-27 17:49:55 -0700

    process input

--- src/find_political_donors.py
+++ src/find_political_donors.py
@@ -2,10 +2,44 @@
 from median import Median
 import datetime
 
+
 # parse the relevant fields from line
 def process_data(line):
+    # if valid for CMTE_ID, TRANSACTION_AMT, or OTHER_ID
+    other_valid = False
+
+    # if TRANSACTION_DT valid
+    date_valid = False
+
+    # if zip_code valid
+    zip_valid = False
+
     fields = line.strip().split('|')
-    return fields[0], fields[10][:5], fields[13], int(fields[14]), fields[15]
+    cmte, zip, date, amt, id = fields[0], fields[10], fields[13], int(fields[14]), fields[15]
+    if cmte and not id:
+        other_valid = True
+    else:
+        return [cmte, zip, date, amt], other_valid, date_valid, zip_valid
+
+    # make sure amt is valid integer
+    try:
+        amt = int(amt)
+    except ValueError:
+        other_valid = False
+
+    # convert date string to datetime object, and check if it's malformed
+    try:
+        dt = datetime.datetime.strptime(date, "%m%d%Y")
+        date_valid = True
+    except ValueError:
+        dt = None
+
+    # parse zip code
+    if len(zip) >= 5:
+        zip_valid = True
+        zip = zip[:5]
+
+    return [cmte, zip, dt, amt], other_valid, date_valid, zip_valid
 
 
 # process median_by_zip file 
@@ -13,7 +47,7 @@ def median_by_zip(dict_by_zip, cmte, zip, amt, out):
     # get the zip dictionary according to the recipient id 
     zip_dict = dict_by_zip.get(cmte, None)
     
-    # if dict_by_zip already has the recipient record
+    # if dict_by_zip already has the recipient transaction
     if zip_dict:
         # get the median object according to zip code 
         median = zip_dict.get(zip, None)
@@ -23,7 +57,7 @@ def median_by_zip(dict_by_zip, cmte, zip, amt, out):
             ready_to_print_num = median.get_num()
             ready_to_print_total = median.get_total()
         else:
-            # if this recipient no this zip code record, then init a new Median obj for this zip code
+            # if this recipient no this zip code transaction, then init a new Median obj for this zip code
             median = Median()
             median.add_num(amt)
             zip_dict[zip] = median
@@ -31,7 +65,7 @@ def median_by_zip(dict_by_zip, cmte, zip, amt, out):
             ready_to_print_num = median.get_num()
             ready_to_print_total = median.get_total()
     else:
-        # dict_by_zip no this recipient record, init a new zip dictionary relate to this recipient id
+        # dict_by_zip no this recipient transaction, init a new zip dictionary relate to this recipient id
         # and init a new median obj, add the value
         zip_dict = {}
         median = Median()
@@ -46,17 +80,15 @@ def median_by_zip(dict_by_zip, cmte, zip, amt, out):
     out.write("{} | {} | {} | {} | {}\n".format(cmte, zip, ready_to_print_median, ready_to_print_num, ready_to_print_total))
 
 
-def median_by_date(dict_by_date, cmte, date, amt):
-    # get the records of this recipient
+def median_by_date(dict_by_date, cmte, dt, amt):
+    # get the transactions of this recipient
     date_dict = dict_by_date.get(cmte, None)
 
-    # convert date string to datetime object in order to sort
-    dt = datetime.datetime.strptime(date, "%m%d%Y")
-
+    # if already have transactions link to this recipient
     if date_dict:
         median = date_dict.get(dt, None)
 
-        # if the record relate to this recipient at this date already be stored
+        # if the transaction to this recipient at this date has already been stored
         if median:
             median.add_num(amt)
         else:
@@ -65,7 +97,7 @@ def median_by_date(dict_by_date, cmte, date, amt):
             median.add_num(amt)
             date_dict[dt] = median
     else:
-        # no records at this date have been found, then init a new dictionary for this date
+        # no transactions made at this date, then init a new dictionary for this date
         date_dict = {}
         median = Median()
         median.add_num(amt)
@@ -77,7 +109,6 @@ def median_by_date(dict_by_date, cmte, date, amt):
 def print_median_by_date(dict_by_date, out):
     # sorted alphabetical by recipient
     for cmte, v in sorted(dict_by_date.items()):
-
         # sorted chronologically by date
         for dt, median in sorted(v.items()):
             date = dt.strftime("%m%d%Y")
@@ -87,12 +118,19 @@ def print_median_by_date(dict_by_date, out):
             out.write("{} | {} | {} | {} | {}\n".format(cmte, date, ready_to_print_median, ready_to_print_num, ready_to_print_total))
 
 
-with open('../input/test') as f, open('../output/test1', 'w') as out, open('../output/test2', 'w') as out1:
-    dict_by_zip = {}
-    dict_by_date = {}
-    for line in f:
-        cmte, zip, dt, amt, id = process_data(line)
-        if not id:
-            median_by_zip(dict_by_zip, cmte, zip, amt, out)
-            median_by_date(dict_by_date, cmte, dt, amt)
-    print_median_by_date(dict_by_date, out1)
+def main():
+    with open('../input/test') as f, open('../output/test1', 'w') as out, open('../output/test2', 'w') as out1:
+        dict_by_zip = {}
+        dict_by_date = {}
+        for line in f:
+            data, other_valid, date_valid, zip_valid = process_data(line)
+            cmte, zip, dt, amt = data
+            if other_valid:
+                if zip_valid:
+                    median_by_zip(dict_by_zip, cmte, zip, amt, out)
+                if date_valid:
+                    median_by_date(dict_by_date, cmte, dt, amt)
+        print_median_by_date(dict_by_date, out1)
+
+if __name__ == '__main__':
+    main()
